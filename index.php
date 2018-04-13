b1530b51be305cfbcbcfb16788527efc51ea5643
grayson <grayson@d4am.com>
2018-04-13 10:37:05 -0700

    Initial Setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: D4 Plugin
+Plugin URI: https://d4am.com/
+Description: Necessary styles and functionality created by D4 Advanced Media
+Version: 1.0
+Author: D4 Adv. Media
+*/
+
+include('config.php');
+include('module/index.php');
+include('system/index.php');
+include('frontend/index.php');
+include('backend/index.php');
